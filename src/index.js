a93bf34ea1bc4040afaae90adb4df52613eafff4
kirillabcd <k.motsart@gmail.com>
2023-08-12 01:24:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // 1. str must be even length
+  if (str.length % 2 > 0 ) {
+    return false
+  }
+
+  let stack = str
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      // 2. replace from stack items from bracketsConfig to empty string.
+      stack = stack.replace(bracketsConfig[j][0] + bracketsConfig[j][1],'')
+    }
+  }
+  //3. if stack after replase is not empty string -> return false / else true.
+  return stack === '' ? true : false
 }
